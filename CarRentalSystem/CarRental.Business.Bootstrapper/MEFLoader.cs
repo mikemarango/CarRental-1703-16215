dd78dbd95385b9d95b99ca7678a38e174c00d8e7
Michael Marango <mike.marango@gmail.com>
2017-06-12 07:53:07 -0700

    added datacontext, datarepositories and mef loader

---
+++ CarRentalSystem/CarRental.Business.Bootstrapper/MEFLoader.cs
@@ -0,0 +1,24 @@
+﻿using CarRental.Data.DataRepositories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Business.Bootstrapper
+{
+    public static class MEFLoader
+    {
+        public static CompositionContainer Init()
+        {
+            AggregateCatalog catalog = new AggregateCatalog();
+
+            catalog.Catalogs.Add(new AssemblyCatalog(typeof(AccountRepository).Assembly));
+
+            CompositionContainer container = new CompositionContainer(catalog);
+
+            return container;
+        }
+    }
+}
---
+++ CarRentalSystem/CarRental.Data/DataContexts/CarRentalContext.cs
@@ -0,0 +1,46 @@
+﻿using CarRental.Business.Entities;
+using Core.Common.Contracts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Data.DataContexts
+{
+    public class CarRentalContext : DbContext
+    {
+        public CarRentalContext()
+            : base("name=CarRental")
+        {
+            Database.SetInitializer<CarRentalContext>(null);
+        }
+
+        public DbSet<Account> AccountSet { get; set; }
+
+        public DbSet<Car> CarSet { get; set; }
+
+        public DbSet<Rental> RentalSet { get; set; }
+
+        public DbSet<Reservation> ReservationSet { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Ignore<PropertyChangedEventHandler>();
+            modelBuilder.Ignore<ExtensionDataObject>();
+            modelBuilder.Ignore<IIdentifiableEntity>();
+
+            modelBuilder.Entity<Account>().HasKey(e => e.AccountId).Ignore(e => e.EntityId);
+            modelBuilder.Entity<Car>().HasKey(e => e.CarId).Ignore(e => e.EntityId);
+            modelBuilder.Entity<Rental>().HasKey(e => e.RentalId).Ignore(e => e.EntityId);
+            modelBuilder.Entity<Reservation>().HasKey(e => e.ReservationId).Ignore(e => e.EntityId);
+            modelBuilder.Entity<Car>().Ignore(e => e.CurrentlyRented);
+        }
+    }
+}
