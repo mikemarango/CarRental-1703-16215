8c65b8244ad9406f09e779ca34158cea2fe048f3
Michael Marango <mike.marango@gmail.com>
2017-06-13 00:24:25 -0700

    added car rental business contracts

---
+++ CarRentalSystem/CarRental.Business.Contracts/ServiceContracts/IIventoryService.cs
@@ -0,0 +1,33 @@
+﻿using CarRental.Business.Entities;
+using Core.Common.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Business.Contracts.ServiceContracts
+{
+    [ServiceContract]
+    public interface IInventoryService
+    {
+        [OperationContract]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        Car UpdateCar(Car car);
+
+        [OperationContract]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        void DeleteCar(int carId);
+
+        [OperationContract]
+        [FaultContract(typeof(NotFoundException))]
+        Car GetCar(int carId);
+
+        [OperationContract]
+        Car[] GetAllCars();
+
+        [OperationContract]
+        Car[] GetAvailableCars(DateTime pickupDate, DateTime returnDate);
+    }
+}
