8d9ae9c513b3caeeefe8faa7a63f71a589eb71f6
Michael Marango <mike.marango@gmail.com>
2017-06-13 00:03:37 -0700

    car rental common and related interfaces

---
+++ CarRentalSystem/CarRental.Common/CarCurrentlyRentedException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Common
+{
+    public class CarCurrentlyRentedException : ApplicationException
+    {
+        public CarCurrentlyRentedException(string message)
+            : base(message)
+        {
+        }
+
+        public CarCurrentlyRentedException(string message, Exception ex)
+            : base(message, ex)
+        {
+        }
+    }
+}
---
+++ CarRentalSystem/CarRental.Common/CarNotRentedException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Common
+{
+    public class CarNotRentedException : ApplicationException
+    {
+        public CarNotRentedException(string message)
+            : base(message)
+        {
+        }
+
+        public CarNotRentedException(string message, Exception ex)
+            : base(message, ex)
+        {
+        }
+    }
+}
