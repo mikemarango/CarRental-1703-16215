0f693fa2ec702b59f0c78fef30e32aa17a2559a7
Michael Marango <mike.marango@gmail.com>
2017-06-14 12:01:56 -0700

    enhanced console app

--- CarRentalSystem/CarRental.ServiceHosts.Console/Program.cs
+++ CarRentalSystem/CarRental.ServiceHosts.Console/Program.cs
@@ -16,13 +16,44 @@ namespace CarRental.ServiceHosts.Console
             WriteLine("Starting services...");
             WriteLine(string.Empty);
 
-            ServiceHost host = new ServiceHost(typeof(InventoryManager));
-            host.Open();
+            ServiceHost hostInventoryManager = new ServiceHost(typeof(InventoryManager));
+            ServiceHost hostRentalManager = new ServiceHost(typeof(RentalManager));
+            ServiceHost hostAccountManager = new ServiceHost(typeof(AccountManager));
+
+            StartService(hostInventoryManager, "InventoryManager");
+            StartService(hostRentalManager, "RentalManger");
+            StartService(hostAccountManager, "AccountManger");
+
 
             WriteLine(string.Empty);
             WriteLine("Press [Enter] to exit.");
-
             ReadKey();
+
+            StopService(hostInventoryManager, "InventoryManager");
+            StopService(hostRentalManager, "RentalManager");
+            StopService(hostAccountManager, "AccountManager");
+        }
+
+        static void StartService(ServiceHost host, string serviceDescription)
+        {
+            host.Open();
+            WriteLine($"Service {serviceDescription} started");
+
+            foreach (var endPoint in host.Description.Endpoints)
+            {
+                WriteLine($"Listening on endpoint:");
+                WriteLine($"Address: {endPoint.Address.Uri.ToString()}");
+                WriteLine($"Binding: {endPoint.Binding.Name}");
+                WriteLine($"Contract: {endPoint.Contract.ConfigurationName}");
+            }
+
+            WriteLine();
+        }
+
+        static void StopService(ServiceHost host, string serviceDescription)
+        {
+            host.Close();
+            WriteLine("Service {0} stopped.", serviceDescription);
         }
     }
 }
