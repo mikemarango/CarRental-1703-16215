813051cd2b1ee9e23c54a89e3c7c9cb782b5373b
Michael Marango <mike.marango@gmail.com>
2017-06-15 11:30:25 -0700

    car client proxies

---
+++ CarRentalSystem/CarRental.Client.Proxies/ServiceProxies/AccountClient.cs
@@ -0,0 +1,41 @@
+﻿using CarRental.Client.Contracts.ServiceContracts;
+using CarRental.Client.Entities;
+using Core.Common.ServiceModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Client.Proxies.ServiceProxies
+{
+    [Export(typeof(IAccountService))]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public class AccountClient : UserClientBase<IAccountService>, IAccountService
+    {
+        public Account GetCustomerAccountInfo(string loginEmail)
+        {
+            return Channel.GetCustomerAccountInfo(loginEmail);
+        }
+
+        public void UpdateCustomerAccountInfo(Account account)
+        {
+            Channel.UpdateCustomerAccountInfo(account);
+        }
+
+        #region Async operations
+
+        public Task<Account> GetCustomerAccountInfoAsync(string loginEmail)
+        {
+            return Channel.GetCustomerAccountInfoAsync(loginEmail);
+        }
+
+        public Task UpdateCustomerAccountInfoAsync(Account account)
+        {
+            return Channel.UpdateCustomerAccountInfoAsync(account);
+        }
+
+        #endregion
+    }
+}
