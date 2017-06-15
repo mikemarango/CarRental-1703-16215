a1bd3968e9f5f1af7c69e7d0c016536909375b63
Michael Marango <mike.marango@gmail.com>
2017-06-15 11:05:28 -0700

    added client related contracts

---
+++ CarRentalSystem/CarRental.Client.Contracts/ServiceContracts/IInventoryService.cs
@@ -0,0 +1,53 @@
+﻿using CarRental.Client.Entities;
+using Core.Common.Contracts;
+using Core.Common.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Client.Contracts.ServiceContracts
+{
+    [ServiceContract]
+    public interface IInventoryService : IServiceContract
+    {
+        [OperationContract]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        Car UpdateCar(Car car);
+
+        [OperationContract]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        void DeleteCar(int carId);
+
+        [OperationContract]
+        [FaultContract(typeof(NotFoundException))]
+        Car GetCar(int carId);
+
+        [OperationContract]
+        Car[] GetAllCars();
+
+        [OperationContract]
+        Car[] GetAvailableCars(DateTime pickupDate, DateTime returnDate);
+
+        #region Async operations
+
+        [OperationContract]
+        Task<Car> UpdateCarAsync(Car car);
+
+        [OperationContract]
+        Task DeleteCarAsync(int carId);
+
+        [OperationContract]
+        Task<Car> GetCarAsync(int carId);
+
+        [OperationContract]
+        Task<Car[]> GetAllCarsAsync();
+
+        [OperationContract]
+        Task<Car[]> GetAvailableCarsAsync(DateTime pickupDate, DateTime returnDate);
+
+        #endregion
+    }
+}
