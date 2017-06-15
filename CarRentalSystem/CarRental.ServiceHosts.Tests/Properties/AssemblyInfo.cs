f4f8e2362d36e68c88381af11fa770a0aa2bf48e
Michael Marango <mike.marango@gmail.com>
2017-06-15 07:42:16 -0700

    completed service access tests

---
+++ CarRentalSystem/CarRental.ServiceHosts.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CarRental.ServiceHosts.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CarRental.ServiceHosts.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9593d001-09b5-4d12-a82a-ff25569c862a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CarRentalSystem/CarRental.ServiceHosts.Tests/ServiceAccessTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ServiceModel;
+using CarRental.Business.Contracts.ServiceContracts;
+
+namespace CarRental.ServiceHosts.Tests
+{
+    [TestClass]
+    public class ServiceAccessTests
+    {
+        [TestMethod]
+        public void test_inventory_manager_as_service()
+        {
+            ChannelFactory<IInventoryService> channelFactory =
+                new ChannelFactory<IInventoryService>(string.Empty);
+
+            IInventoryService proxy = channelFactory.CreateChannel();
+
+            (proxy as ICommunicationObject).Open();
+
+            channelFactory.Close();
+        }
+
+        [TestMethod]
+        public void test_rental_manager_as_service()
+        {
+            ChannelFactory<IRentalService> channelFactory =
+                new ChannelFactory<IRentalService>(string.Empty);
+
+            IRentalService proxy = channelFactory.CreateChannel();
+
+            (proxy as ICommunicationObject).Open();
+
+            channelFactory.Close();
+        }
+
+        [TestMethod]
+        public void test_account_manager_as_service()
+        {
+            ChannelFactory<IAccountService> channelFactory =
+                new ChannelFactory<IAccountService>(string.Empty);
+
+            IAccountService proxy = channelFactory.CreateChannel();
+
+            (proxy as ICommunicationObject).Open();
+
+            channelFactory.Close();
+        }
+
+    }
+}
