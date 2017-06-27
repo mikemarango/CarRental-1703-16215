78be2148ba1bb7cb14b4f7af80db82037cc953f5
Michael Marango <mike.marango@gmail.com>
2017-06-27 03:41:50 -0700

    added javascript models and viewmodels

---
+++ CarRentalSystem/CarRental.Web/Scripts/Bindings/Models/AccountChangePasswordModel.js
@@ -0,0 +1,9 @@
+﻿(function (cr) {
+    var AccountChangePasswordModel = function () {
+        var self = this;
+        self.LoginEmail = '';
+        self.OldPassword = '';
+        self.NewPassword = '';
+    }
+    cr.AccountChangePasswordModel = AccountChangePasswordModel;
+}(window.CarRental));
