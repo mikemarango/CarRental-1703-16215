32ebd3e5811fc6935aa27168bf814888b2168343
Michael Marango <mike.marango@gmail.com>
2017-06-27 07:49:06 -0700

    account views and related code

--- CarRentalSystem/CarRental.Web/Core/MefDependencyResolver.cs
+++ CarRentalSystem/CarRental.Web/Core/MefDependencyResolver.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition.Hosting;
+using Core.Common.Extensions;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace CarRental.Web.Core
 {
-    public class MefDependencyResolver
+    public class MefDependencyResolver : IDependencyResolver
     {
+        public MefDependencyResolver(CompositionContainer container)
+        {
+            _Container = container;
+        }
+
+        CompositionContainer _Container;
+
+        public object GetService(Type serviceType)
+        {
+            return _Container.GetExportedValueByType(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _Container.GetExportedValuesByType(serviceType);
+        }
     }
 }
--- CarRentalSystem/CarRental.Web/Core/UsesDisposableServiceAttribute.cs
+++ CarRentalSystem/CarRental.Web/Core/UsesDisposableServiceAttribute.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
 
 namespace CarRental.Web.Core
 {
-    public class UsesDisposableServiceAttribute
+    public class UsesDisposableServiceAttribute : ActionFilterAttribute
     {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            // pre-processing
+
+            if (actionContext.ControllerContext.Controller is IServiceAwareController controller)
+            {
+                controller.RegisterDisposableServices(controller.DisposableServices);
+            }
+        }
+
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            //post-processing
+
+            if (actionExecutedContext.ActionContext.ControllerContext.Controller is IServiceAwareController controller)
+            {
+                foreach (var service in controller.DisposableServices)
+                {
+                    if (service != null && service is IDisposable)
+                        (service as IDisposable).Dispose();
+                }
+            }
+        }
     }
 }
